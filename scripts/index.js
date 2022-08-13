d7308458f184a7feafc501c14cbff7ea1b16cd53
Elena Andreeva <aes1999@yandex.ru>
2022-08-13 19:38:16 +0500

    added fife-project

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 const popupButtonEdit = document.querySelector(".profile__button-edit");
 const popupProfile = document.querySelector(".popup");
 const formElement = document.querySelector(".popup__form");
@@ -6,6 +7,18 @@ const captionInput = document.querySelector(".popup__input_type_caption");
 const profileName = document.querySelector(".profile__name");
 const profileCaption = document.querySelector(".profile__caption");
 const closeEditButton = document.querySelector(".popup__button-close");
+const gallery = document.querySelector(".gallery");
+const likeButton = document.querySelector(".gallery__button-like");
+const popupPhoto = document.querySelector(".popup_photo-zoom");
+const popupImg = document.querySelector(".popup_photo-zoom__img");
+const popupTitle = document.querySelector(".popup_photo-zoom__title");
+const closePopup = document.querySelector(".popup__button-close_type_photo-zoom");
+const newElemName = document.querySelector(".popup__input_type_place-name");
+const newElemLink = document.querySelector(".popup__input_type_link");
+const addGalleryElemForm = document.querySelector(".popup__form_gallery_change");
+const addElemButtonClose = document.querySelector(".popup__button-close-gallery");
+const popupGallery = document.querySelector(".popup-gallery");
+const addElemButton = document.querySelector(".profile__add-button");
 
 
 function openEditProfile() {
@@ -13,6 +26,7 @@ function openEditProfile() {
     captionInput.value = profileCaption.textContent;
     popupProfile.classList.add("popup_is-opened");
 }
+
 
 function closeEditProfile() {
     popupProfile.classList.remove("popup_is-opened");
@@ -28,5 +42,97 @@ function formSubmitHandler (evt) {
 popupButtonEdit.addEventListener("click", openEditProfile)
 closeEditButton.addEventListener("click", closeEditProfile)
 formElement.addEventListener('submit', formSubmitHandler); 
+
+function createElement(name, link) {
+  const template = document.querySelector("#template-form").content.querySelector(".gallery__element").cloneNode(true);
+  const imgElem = template.querySelector(".gallery__photo");
+  const titleElem = template.querySelector(".gallery__element-title");
+  const likeButton = template.querySelector(".gallery__button-like");
+  const deleteButton = template.querySelector(".gallery__button-delete");
+  imgElem.setAttribute("src",link);
+  titleElem.textContent = name;
+  
+  
+
+  likeButton.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("gallery__button-like_active")
+  })
+
+  deleteButton.addEventListener("click", function(evt) {
+    const eventTarget = evt.target;
+    const elem = eventTarget.closest('.gallery__element');
+    elem.remove();
+  })
+
+  imgElem.addEventListener("click", function(evt) {
+    const eventTarget = evt.target;
+    popupImg.setAttribute('src',eventTarget.src);
+    popupTitle.textContent = titleElem.textContent;
+    popupPhoto.classList.add("popup_is-opened");
+  })
+
+  closePopup.addEventListener("click", function() {
+    popupPhoto.classList.remove("popup_is-opened");
+  } )
+
+  return template;
+}
+
+function addNewElement (evt) {
+  evt.preventDefault();
+  const eventTarget = evt.target.closest('.popup');
+  gallery.prepend(createElement(newElemName.value, newElemLink.value));
+  evt.target.reset()
+  popupGallery.classList.remove("popup_is-opened")
+}
+
+addGalleryElemForm.addEventListener("submit", addNewElement)
+
+addElemButtonClose.addEventListener("click", function() {
+  popupGallery.classList.remove("popup_is-opened");
+})
+
+addElemButton.addEventListener("click", function() {
+  popupGallery.classList.add("popup_is-opened");
+})
+
+function addInitialGallery() {
+  const initialCards = [
+  {
+  name: 'Архыз',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+},
+{
+  name: 'Челябинская область',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+},
+{
+  name: 'Иваново',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+},
+{
+  name: 'Камчатка',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+},
+{
+  name: 'Холмогорский район',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+},
+{
+  name: 'Байкал',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+}
+];  
+
+  initialCards.forEach(function (item){
+      const elemName = item.name;
+      const elemLink = item.link;
+      gallery.prepend(createElement(elemName, elemLink));
+  })
+
+}
+
+addInitialGallery();
+
 
 
