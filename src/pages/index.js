c393118dc168bf6c8bc7e5b52375fb427a7727a7
Elena Andreeva <aes1999@yandex.ru>
2022-11-12 09:58:46 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,10 @@ import {
   buttonConfirm,
   gallerySaveButton,
   inputName,
-  inputCaption
+  inputCaption,
 } from "../utils/constants.js";
 
-import {renderLoading} from "../utils/utils.js"
+import { renderLoading } from "../utils/utils.js";
 
 import { PopupWithImage } from "../components/PopupWithImage.js";
 import { Card } from "../components/Card.js";
@@ -26,41 +26,43 @@ import { UserInfo } from "../components/UserInfo.js";
 import { Api } from "../components/Api.js";
 import { PopupDelete } from "../components/PopupDelete.js";
 
-
-const api = new Api({baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-52',
-headers: {
-  authorization: '9fcfb2cc-6788-4de3-958e-87d26868b61e',
-  'Content-Type': 'application/json'
-}});
+const api = new Api({
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-52",
+  headers: {
+    authorization: "9fcfb2cc-6788-4de3-958e-87d26868b61e",
+    "Content-Type": "application/json",
+  },
+});
 
 Promise.all([api.getInitialGallery(), api.getInitialProfileData()])
-.then(([cardsData, profileData]) => {
-  profileUserInfo.setUserInfo(profileData);
-  for (let i = 0; i < cardsData.length; i++) {
-    if (cardsData[i].owner._id !== profileData._id) {
-      cardsData[i].isMy = false
+  .then(([cardsData, profileData]) => {
+    profileUserInfo.setUserInfo(profileData);
+    for (let i = 0; i < cardsData.length; i++) {
+      if (cardsData[i].owner._id !== profileData._id) {
+        cardsData[i].isMy = false;
+      }
+      if (
+        cardsData[i].likes
+          .map((likeData) => likeData._id)
+          .includes(profileData._id) === true
+      ) {
+        cardsData[i].isLiked = true;
+      }
     }
-   if (cardsData[i].likes.map((likeData) => likeData._id).includes(profileData._id) === true) {
-    cardsData[i].isLiked = true;
-   }
-  }
-  return cardsData
-})
-  .then((cardsData) => 
-    {
-      renderListCards.renderItems(cardsData)}
-  )
-  .catch(err => {
-    console.log(err)
+    return cardsData;
+  })
+  .then((cardsData) => {
+    renderListCards.renderItems(cardsData);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-
 
 const handleCardLikeCounter = api.setLikes.bind(api);
 
 const renderListCards = new Section(
   {
-    renderer: (item)  => {
+    renderer: (item) => {
       const cardElem = createCard(item);
       renderListCards.addItem(cardElem);
     },
@@ -71,9 +73,8 @@ const renderListCards = new Section(
 const profileUserInfo = new UserInfo({
   userName: ".profile__name",
   userDescription: ".profile__caption",
-  userAvatar: ".profile__photo"
+  userAvatar: ".profile__photo",
 });
-
 
 const popupGallery = new PopupWithForm(".popup_gallery", {
   handleFormSubmit: () => {
@@ -89,7 +90,7 @@ const popupGallery = new PopupWithForm(".popup_gallery", {
       })
       .finally(() => {
         renderLoading(false, gallerySaveButton, "Сохранение...", "Сохранить");
-      })
+      });
   },
 });
 
@@ -99,16 +100,17 @@ const popupAvatar = new PopupWithForm(".popup_avatar", {
     api
       .setNewAvatar(popupAvatar.getInputValues())
       .then((result) => {
-        profileUserInfo.setUserInfo(result)
+        profileUserInfo.setUserInfo(result);
       })
       .then(() => popupAvatar.close())
+      .catch((err) => {
+        console.log(err);
+      })
       .finally(() => {
         renderLoading(false, buttonSaveAvatar, "Сохранение...", "Сохранить");
-        
-      })
+      });
   },
 });
-
 
 const popupProfile = new PopupWithForm(".popup_profile", {
   handleFormSubmit: () => {
@@ -128,16 +130,14 @@ const popupProfile = new PopupWithForm(".popup_profile", {
   },
 });
 
-
-
 const popupDeleteConfirm = new PopupDelete(".popup_delete-confirm", {
   handleFormSubmit: (cardId, element) => {
     renderLoading(true, buttonConfirm, "Подождите...", "Да");
 
     return api
       .deleteCard(cardId)
-      .then(element.remove())
-      .then(popupDeleteConfirm.close())
+      .then(() => element.remove())
+      .then(() => popupDeleteConfirm.close())
       .catch((err) => {
         console.log(err);
       })
@@ -147,19 +147,11 @@ const popupDeleteConfirm = new PopupDelete(".popup_delete-confirm", {
   },
 });
 
-function resetValidationOnPopup(popup) {
-  popup.resetValidation();
-}
-
-
 const profileValidator = new FormValidator(
   initialConfig,
   ".popup__form-profile"
 );
-const popupAvatarValidator = new FormValidator(
-  initialConfig,
-  ".popup_avatar"
-);
+const popupAvatarValidator = new FormValidator(initialConfig, ".popup_avatar");
 
 const elemGalleryValidator = new FormValidator(
   initialConfig,
@@ -171,36 +163,28 @@ profileValidator.enableValidation();
 profileValidator.enablePopupSubmitButton();
 popupAvatarValidator.enableValidation();
 
-
-
-
-function createCard (item) {
+function createCard(item) {
   const addCardsList = new Card(
     item,
     handleCardClick,
     handleCardLikeCounter,
     handleClickOpenPopupDelete,
-    "#template-form",
-    
+    "#template-form"
   );
   const cardElement = addCardsList.generateElem(item);
   return cardElement;
-};
+}
 const popupPhotoZoom = new PopupWithImage(".popup_photo-zoom");
 
 function handleCardClick(name, link) {
   popupPhotoZoom.open(name, link);
 }
 
-
 function handleClickOpenPopupDelete(cardId, card) {
   popupDeleteConfirm.card = card;
   popupDeleteConfirm.cardId = cardId;
   popupDeleteConfirm.open();
-  
 }
-
-
 
 popupButtonEdit.addEventListener("click", () => {
   const data = profileUserInfo.getUserInfo();
@@ -213,9 +197,7 @@ popupGallery.setEventListeners();
 popupDeleteConfirm.setEventListeners();
 popupAvatar.setEventListeners();
 popupProfile.setEventListeners();
-popupPhotoZoom.setEventListeners()
-
-
+popupPhotoZoom.setEventListeners();
 
 buttonAddElem.addEventListener("click", () => {
   elemGalleryValidator.resetValidation();
@@ -223,9 +205,6 @@ buttonAddElem.addEventListener("click", () => {
 });
 
 buttonChangeAvatar.addEventListener("click", () => {
-  popupAvatarValidator.resetValidation()
+  popupAvatarValidator.resetValidation();
   popupAvatar.open();
-
-  
-  
 });
